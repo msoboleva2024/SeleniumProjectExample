cfcb679aa44d70442500a39d4206f44b998d7f78
msobolev <masha2005.88@gmail.com>
2025-01-15 08:38:58 +0100

    AfterMorningBranch second  comment

--- src/main/java/PageObject/Checkout_OverviewPage.java
+++ src/main/java/PageObject/Checkout_OverviewPage.java
@@ -201,7 +201,7 @@ public class Checkout_OverviewPage extends AbstractComponent {
 						cartItems.get(i).findElement(By.className("inventory_item_name")).click();
 						return new YourCartPage(driver);
 						}
-					
+					//secondComment - AfterMorningBranch 08:38
 			}
 			return null;
 }
